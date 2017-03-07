1710b50cfea09a8b55925c6c715ce837e71a013d
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-03-07 18:45:23 +0000

    Refactored AssiduityController in order to remove DB connection

--- app/Http/Controllers/AssiduityController.php
+++ app/Http/Controllers/AssiduityController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Assiduity;
 use App\Http\Controllers\SOAPController;
 use App\Http\Controllers\MessagesController;
 
@@ -18,50 +17,30 @@ class AssiduityController extends Controller {
      *
      * @return void
      */
-    public function __construct(Request $request, SOAPController $soap, MessagesController $message, Assiduity $assiduity) {
+    public function __construct(Request $request, SOAPController $soap, MessagesController $message) {
         $this->apiToken = $request->input("token");
         $this->soap = $soap;
         $this->message = $message;
-        $this->assiduity = $assiduity;
     }
 
     public function getAssiduity() {
         $tokenData = (object) $this->message->decryptToken($this->apiToken);
 
         if($tokenData->token) {
-            if(!$this->hasUserAssiduityDetails($tokenData->number)) {
-                $assiduityAux = $this->soap->getDataFromSOAPServer("assiduity", array("assiduity" => array("token" => $tokenData->token)));
+            $assiduityAux = $this->soap->getDataFromSOAPServer("assiduity", array("assiduity" => array("token" => $tokenData->token)));
 
-                if(property_exists(json_decode($assiduityAux->assiduityResult), "Error"))
-                    return $this->message->encodeMessage(1, "Invalid token");
+            if(property_exists(json_decode($assiduityAux->assiduityResult), "Error"))
+                return $this->message->encodeMessage(1, "Invalid token");
 
-                $assiduity = array();
+            $assiduity = array();
 
-                foreach(json_decode($assiduityAux->assiduityResult)->assiduity as $detail) {
-                    array_push($assiduity, array("unidade" => $detail->Unidade, "tipo" => $detail->Tipo, "assiduidade" => $detail->Assiduidade));
-                }
-
-                if (!empty($assiduity)) {
-                    $userAssiduity = new $this->assiduity;
-
-                    $userAssiduity->number = $tokenData->number;
-                    $userAssiduity->assiduity = $assiduity;
-
-                    $userAssiduity->save();
-
-                    return $this->message->encodeMessage(0, $userAssiduity->assiduity);
-                }
-
-                return $this->message->encodeMessage(1, "No assiduity information found");
+            foreach(json_decode($assiduityAux->assiduityResult)->assiduity as $detail) {
+                array_push($assiduity, array("unidade" => $detail->Unidade, "tipo" => $detail->Tipo, "assiduidade" => $detail->Assiduidade));
             }
 
-            return $this->message->encodeMessage(0, $this->assiduity->where("number", "=", $tokenData->number)->first()->assiduity);
+            return (!empty($assiduity)) ? $this->message->encodeMessage(0, $assiduity) : $this->encodeMessage(1, "No assiduity information found");
         }
 
         return $this->message->encodeMessage(1, "Couldn't decrypt sent token");
-    }
-
-    private function hasUserAssiduityDetails($userNumber) {
-        return $this->assiduity->where("number", "=", $userNumber)->exists();
     }
 }
