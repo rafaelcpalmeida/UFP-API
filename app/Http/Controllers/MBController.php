b834b0fc029a24c0c6ac043e53d06cc34ca5875d
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-03-07 19:27:56 +0000

    Refactored MBController in order to remove DB connection

--- app/Http/Controllers/MBController.php
+++ app/Http/Controllers/MBController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Multibanco;
 use App\Http\Controllers\SOAPController;
 use App\Http\Controllers\MessagesController;
 
 class MBController extends Controller {
     private $soap;
     private $message;
-    private $mb;
     private $apiToken;
     
     /**
@@ -18,44 +16,24 @@ class MBController extends Controller {
      *
      * @return void
      */
-    public function __construct(Request $request, SOAPController $soap, MessagesController $message, Multibanco $mb) {
+    public function __construct(Request $request, SOAPController $soap, MessagesController $message) {
         $this->apiToken = $request->input("token");
         $this->soap = $soap;
         $this->message = $message;
-        $this->mb = $mb;
     }
 
     public function getMB() {
         $tokenData = (object) $this->message->decryptToken($this->apiToken);
 
         if($tokenData->token) {
-            if(!$this->hasUserMBDetails($tokenData->number)) {
-                $mbDetails = $this->soap->getDataFromSOAPServer("atm", array("atm" => array("token" => $tokenData->token)));
+            $mbDetails = $this->soap->getDataFromSOAPServer("atm", array("atm" => array("token" => $tokenData->token)));
 
-                if(property_exists(json_decode($mbDetails->atmResult), "Error"))
-                    return $this->message->encodeMessage(1, "Invalid token");
-                
-                if((isset(json_decode($mbDetails->atmResult)->atm[0]))) {
-                    $userMB = new $this->mb;
+            if(property_exists(json_decode($mbDetails->atmResult), "Error"))
+                return $this->message->encodeMessage(1, "Invalid token");
 
-                    $userMB->number = $tokenData->number;
-                    $userMB->mbDetails = json_decode($mbDetails->atmResult)->atm[0];
-
-                    $userMB->save();
-
-                    return $this->message->encodeMessage(0, $userMB->mbDetails);
-                }
-
-                return $this->message->encodeMessage(1, "No payment information found");
-            }
-
-            return $this->message->encodeMessage(0, $this->mb->where("number", "=", $tokenData->number)->first()->mbDetails);
+            return (isset(json_decode($mbDetails->atmResult)->atm[0])) ? $this->message->encodeMessage(0, $userMB->mbDetails) : $this->message->encodeMessage(1, "No payment information found");
         }
         
         return $this->message->encodeMessage(1, "Couldn't decrypt sent token");
-    }
-
-    private function hasUserMBDetails($userNumber) {
-        return $this->mb->where("number", "=", $userNumber)->exists();
     }
 }
