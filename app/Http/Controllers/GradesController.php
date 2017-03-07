beac2920af32a03d249aa136f49d148dc8e2cb5a
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-03-07 19:04:54 +0000

    Refactored GradesController in order to remove DB connection

--- app/Http/Controllers/GradesController.php
+++ app/Http/Controllers/GradesController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\FinalGrades;
-use App\DetailedGrades;
 use App\Http\Controllers\SOAPController;
 use App\Http\Controllers\MessagesController;
 
@@ -20,41 +18,25 @@ class GradesController extends Controller {
      *
      * @return void
      */
-    public function __construct(Request $request, SOAPController $soap, MessagesController $message, FinalGrades $finalGrades, DetailedGrades $detailedGrades) {
+    public function __construct(Request $request, SOAPController $soap, MessagesController $message) {
         $this->apiToken = $request->input("token");
         $this->soap = $soap;
         $this->message = $message;
         $this->finalGrades = $finalGrades;
-        $this->detailedGrades = $detailedGrades;
     }
 
     public function getFinalGrades() {
         $tokenData = (object) $this->message->decryptToken($this->apiToken);
 
         if($tokenData->token) {
-            if(!$this->hasUserFinalGradesDetails($tokenData->number)) {
-                $gradesAux = $this->soap->getDataFromSOAPServer("grade", array("grade" => array("token" => $tokenData->token)));
+            $gradesAux = $this->soap->getDataFromSOAPServer("grade", array("grade" => array("token" => $tokenData->token)));
+            
+            if(property_exists(json_decode($gradesAux->gradeResult), "Error"))
+                return $this->message->encodeMessage(1, "Invalid token");
+            
+            $finalGrades = $this->parseFinalGrades(json_decode($gradesAux->gradeResult)->grade->definitivo);
 
-                if(property_exists(json_decode($gradesAux->gradeResult), "Error"))
-                    return $this->message->encodeMessage(1, "Invalid token");
-
-                $finalGrades = $this->parseFinalGrades(json_decode($gradesAux->gradeResult)->grade->definitivo);
-
-                if (!empty($finalGrades)) {
-                    $userFinalGrades = new $this->finalGrades;
-
-                    $userFinalGrades->number = $tokenData->number;
-                    $userFinalGrades->grades = $finalGrades;
-
-                    $userFinalGrades->save();
-
-                    return $this->message->encodeMessage(0, $userFinalGrades->grades);
-                }
-
-                return $this->message->encodeMessage(1, "No final grades information found");
-            }
-
-            return $this->message->encodeMessage(0, $this->finalGrades->where("number", "=", $tokenData->number)->first()->grades);
+            return (!empty($finalGrades)) ? $this->message->encodeMessage(0, $userFinalGrades->grades) : $this->message->encodeMessage(0, $userFinalGrades->grades);
         }
 
         return $this->message->encodeMessage(1, "Couldn't decrypt sent token");
@@ -64,40 +46,17 @@ class GradesController extends Controller {
         $tokenData = (object) $this->message->decryptToken($this->apiToken);
 
         if($tokenData->token) {
-            if(!$this->hasUserDetailedGradesDetails($tokenData->number)) {
-                $gradesAux = $this->soap->getDataFromSOAPServer("grade", array("grade" => array("token" => $tokenData->token)));
+            $gradesAux = $this->soap->getDataFromSOAPServer("grade", array("grade" => array("token" => $tokenData->token)));
 
-                if(property_exists(json_decode($gradesAux->gradeResult), "Error"))
-                    return $this->message->encodeMessage(1, "Invalid token");
+            if(property_exists(json_decode($gradesAux->gradeResult), "Error"))
+                return $this->message->encodeMessage(1, "Invalid token");
 
-                $detailedGrades = $this->parseDetailedGrades(json_decode($gradesAux->gradeResult)->grade->provisorio->parciais);
-
-                if (!empty($detailedGrades)) {
-                    $userDetailedGrades = new $this->detailedGrades;
-
-                    $userDetailedGrades->number = $tokenData->number;
-                    $userDetailedGrades->grades = $detailedGrades;
-
-                    $userDetailedGrades->save();
-
-                    return $this->message->encodeMessage(0, $userDetailedGrades->grades);
-                }
-
-                return $this->message->encodeMessage(1, "No detailed grades information found");
-            }
-
-            return $this->message->encodeMessage(0, $this->detailedGrades->where("number", "=", $tokenData->number)->first()->grades);
+            $detailedGrades = $this->parseDetailedGrades(json_decode($gradesAux->gradeResult)->grade->provisorio->parciais);
+            
+            return (!empty($detailedGrades)) ? $this->message->encodeMessage(0, $userDetailedGrades->grades) : $this->message->encodeMessage(1, "No detailed grades information found");
         }
 
         return $this->message->encodeMessage(1, "Couldn't decrypt sent token");
-    }
-
-    private function hasUserFinalGradesDetails($userNumber) {
-        return $this->finalGrades->where("number", "=", $userNumber)->exists();
-    }
-
-    private function hasUserDetailedGradesDetails($userNumber) {
-        return $this->detailedGrades->where("number", "=", $userNumber)->exists();
     }
 
     private function parseFinalGrades($grades) {
