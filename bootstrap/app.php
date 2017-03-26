f5b3f2a26b02b45140ce204936591bea976c93a6
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-03-27 00:10:14 +0100

    Remove Mongo dependency

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,8 +23,6 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
-$app->register(Jenssegers\Mongodb\MongodbServiceProvider::class);
-
 $app->withFacades();
 
 $app->withEloquent();
