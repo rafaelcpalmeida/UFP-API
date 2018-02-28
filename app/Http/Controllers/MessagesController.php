88502d643487f022e8599f6bbe3dd76f66834bbb
Joaquim Magalhães <magalhaes1915@gmail.com>
2018-02-28 20:31:43 +0000

    Add encryption option (#2)

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,21 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Contracts\Encryption\Encrypter;
 use Illuminate\Contracts\Encryption\DecryptException;
 
 class MessagesController extends Controller
 {
     private $crypt;
+    private $secure;
     
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(Encrypter $crypt)
+    public function __construct(Request $request, Encrypter $crypt)
     {
         $this->crypt = $crypt;
+        $this->secure = $request->input("secure") ? true : false;
     }
 
     public function decryptToken($encryptedToken)
@@ -30,7 +33,7 @@ class MessagesController extends Controller
 
     public function encodeMessage($status, $message)
     {
-        return response(json_encode(["status" => ($status == 200) ? "Ok" : "Error", "message" => $message]), $status)->header('Content-Type', 'application/json');
+        return response(json_encode(["status" => ($status == 200) ? "Ok" : "Error", "message" => $this->secure ? $this->encryptMessage($message) : $message]), $status)->header('Content-Type', 'application/json');
     }
 
     public function encryptMessage($message)
